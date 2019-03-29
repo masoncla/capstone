724c60c5251f9088cab81189c7f750aad677b649
Claire Mason <c.mason3993@gmail.com>
2019-03-29 11:05:42 -0700

    update encoding script

--- sentence_encode.py
+++ sentence_encode.py
@@ -4,12 +4,16 @@ import numpy as np
 import os
 import pandas as pd
 import requests
-import re
+
 from collections import defaultdict
+import nltk
+nltk.download('punkt')
 from nltk import sent_tokenize
+
 
 def split_sentences(body):
     # tokenize into sentences
+    
     sents = sent_tokenize(body)
     
     # break apart the sentences on the new lines \n
@@ -23,29 +27,48 @@ def split_sentences(body):
     return sents
 
 def sent_engineer(df):
-    body_sents_series = pd.Series([split_sentences(df.body.iloc[i]) for i in range(len(df))])
+    body_sents_series = pd.Series([split_sentences(df.body.iloc[i].decode('utf-8')) for i in range(len(df))])
     body_sents_df = pd.DataFrame(body_sents_series, columns=['body_sents'])
-    inst_sents_series = pd.Series([split_sentences(df.instructions.iloc[i]) for i in range(len(df))])
+    inst_sents_series = pd.Series([split_sentences(df.instructions.iloc[i].decode('utf-8')) for i in range(len(df))])
     inst_sents_df = pd.DataFrame(inst_sents_series, columns = ['inst_sents'])
     data = pd.concat([df, body_sents_df, inst_sents_df], axis=1)
     return data
 
+def get_corr(embeded_inst):
+    final = pd.DataFrame(columns=['sentence', 'correlation'])
+    for i in range(len(embeded_inst)):
+        corr = np.inner(embeded_inst[i][0], embeded_inst[i][1])
+        max_corr = np.array([(i, row.max()) for i, row in enumerate(corr)])
+        sent_df = pd.DataFrame(data.body_sents.iloc[i], columns=['sentence'])
+        array_df = pd.DataFrame(embeded_inst[i][0])
+        rel_df = pd.DataFrame(max_corr[:,1], columns=['correlation'])
+        labelled = pd.concat([sent_df, array_df, rel_df], axis=1)
+        final = pd.concat([final, labelled], ignore_index=True)
+    return final
 
 
-module_url = "https://tfhub.dev/google/universal-sentence-encoder/2" 
-# Import the Universal Sentence Encoder's TF Hub module
-embed = hub.Module(module_url)
 
-# Reduce logging output
-tf.logging.set_verbosity(tf.logging.ERROR)
 
-embeded_inst = defaultdict()
+if __name__=='__main__':
+    data = pd.read_csv('minimal_bake.csv')
+    data = sent_engineer(data)
 
-with tf.Session() as session:
-  session.run([tf.global_variables_initializer(), tf.tables_initializer()])
-  for i in range(80):
-    if i%5 == 0:
-      print(i)
-    #embed_body = session.run(embed(data.body_sents.iloc[i]))
-    embed2 = session.run(embed(data.inst_sents.iloc[i]))
-    embeded_inst[i] = embed2
+
+    module_url = "https://tfhub.dev/google/universal-sentence-encoder/2" 
+    # Import the Universal Sentence Encoder's TF Hub module
+    embed = hub.Module(module_url)
+
+    # Reduce logging output
+    tf.logging.set_verbosity(tf.logging.ERROR)
+
+    embeded = defaultdict()
+
+    with tf.Session() as session:
+        session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+        for i in range(80):
+            embed_body = session.run(embed(data.body_sents.iloc[i]))
+            embed_inst = session.run(embed(data.inst_sents.iloc[i]))
+            embeded[i] = (embed_body, embed_inst)
+
+    final = get_corr(embeded)
+    final.to_csv('vec_data.csv')
