f487ac2279e640e3adabf4b4c17208130352bfc4
Claire Mason <c.mason3993@gmail.com>
2019-04-02 13:58:32 -0700

    change to use TF placeholder and make encoding faster

--- sentence_encode.py
+++ sentence_encode.py
@@ -4,33 +4,11 @@ import numpy as np
 import os
 import pandas as pd
 import requests
+import time
+from sys import argv, exit
 
-from collections import defaultdict
-from nltk import sent_tokenize
 
 
-def split_sentences(body):
-    # tokenize into sentences
-    
-    sents = sent_tokenize(body)
-    
-    # break apart the sentences on the new lines \n
-    new = ''
-    for i, sent in enumerate(sents):
-        if '\n' in sent:
-            new += sents.pop(i)
-    new = [line for line in new.split('\n') if line and ' ' in line] 
-    # create one long list of sentence strings       
-    sents += new
-    return sents
-
-def sent_engineer(df):
-    body_sents_series = pd.Series([split_sentences(df.body.iloc[i].decode('utf-8')) for i in range(len(df))])
-    body_sents_df = pd.DataFrame(body_sents_series, columns=['body_sents'])
-    inst_sents_series = pd.Series([split_sentences(df.instructions.iloc[i].decode('utf-8')) for i in range(len(df))])
-    inst_sents_df = pd.DataFrame(inst_sents_series, columns = ['inst_sents'])
-    data = pd.concat([df, body_sents_df, inst_sents_df], axis=1)
-    return data
 
 def get_corr(embeded_inst, data):
     final = pd.DataFrame(columns=['sentence', 'correlation'])
@@ -46,29 +24,31 @@ def get_corr(embeded_inst, data):
 
 
 
+
 if __name__=='__main__':
-    data = pd.read_csv('minimal_bake.csv')
-    data = sent_engineer(data)
+    data = pd.read_csv(argv[1])
+    sent_batch = data.sentence.values
 
-
+    t2 = time.time()
     module_url = "https://tfhub.dev/google/universal-sentence-encoder/2" 
     # Import the Universal Sentence Encoder's TF Hub module
     embed = hub.Module(module_url)
+    
 
-    # Reduce logging output
-    tf.logging.set_verbosity(tf.logging.ERROR)
+    
 
-    embeded = defaultdict()
+    # create GUSE placeholder
+    sentences = tf.placeholder(dtype=tf.string, shape=[None])
+    embedding = embed(sentences)
 
     with tf.Session() as session:
         session.run([tf.global_variables_initializer(), tf.tables_initializer()])
-        for i in range(len(data)):
-            embed_body = session.run(embed(data.body_sents.iloc[i]))
-            embed_inst = session.run(embed(data.inst_sents.iloc[i]))
-            embeded[i] = (embed_body, embed_inst)
-    # put vector arrays into dataframe
-    D = pd.DataFrame(data=embeded.values(), columns=embeded.keys())
-    D.to_csv('data_vecs.csv')
+        embed_body = session.run(embedding, feed_dict={sentences: sent_batch})
+
+    print('Total time: {}'.format(time.time()-t2))
+    
+
     # put sentences and correlations into dataframe
-    final = get_corr(embeded, data)
-    final.to_csv('corr_data.csv')
+    array_df = pd.DataFrame(embed_body)
+    final = pd.concat([data, array_df], axis=1)
+    final.to_csv(argv[2])
