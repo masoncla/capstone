68d267dfdefc9aa0e1fba792f16412d556c60849
Claire Mason <c.mason3993@gmail.com>
2019-04-03 16:08:01 -0700

    work now?

--- model/model.py
+++ model/model.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np 
 from sklearn.linear_model import LogisticRegression
 import yaml
+import pickle
 
 # get ya data
 df = pd.read_csv('data/train.csv')
@@ -16,5 +17,4 @@ df['relevance'][df['correlation']>=0.75]=1
 with open('/Users/clairemason/Galvanize/Capstone/capstone/model_param.yaml', 'r') as params:
     params = yaml.load(params)
 
-model = LogisticRegression(**params)
-model.fit()
+model = pickle.load(open('model.p', 'rb'))
