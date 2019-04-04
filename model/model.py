1a56b6cf8271ef13b21c4ddb0a30dc210ac336e5
Claire Mason <c.mason3993@gmail.com>
2019-04-04 13:06:39 -0700

    saving model.py changes

--- model/model.py
+++ model/model.py
@@ -3,18 +3,16 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 import yaml
 import pickle
+from label_engineer import get_corr_df
 
 # get ya data
-df = pd.read_csv('data/train.csv')
+df_bodies = pd.read_csv('data/train.csv')
+df_instructions = 
 
 #create labels
 df['relevance']=0
 df['relevance'][df['correlation']>=0.75]=1
 
 
-
-
-with open('/Users/clairemason/Galvanize/Capstone/capstone/model_param.yaml', 'r') as params:
-    params = yaml.load(params)
 
 model = pickle.load(open('model.p', 'rb'))
