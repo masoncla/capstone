573579073322cd64cef31f0791e35f96e0d5a820
Claire Mason <c.mason3993@gmail.com>
2019-04-08 16:23:32 -0700

    add final model

--- model/model.py
+++ model/model.py
@@ -1,24 +1,28 @@
 import pandas as pd 
 import numpy as np 
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegressionCV
 import yaml
 import pickle
+from sys import argv
 
+if __name__=='__main__':
+    # import training data
+    df_train = pd.read_csv(argv[1])
+    df_train.dropna(inplace=True)
+    # split into features and target
+    X_train = df_train.drop(['sentence', 'correlation', 'relevance'], axis=1).values
+    y_train = df_train.relevance.values
 
-# import training data
-df_train = pd.read_csv('train_data_filepath')
-# split into features and target
-X_train = df_train.drop(['sentence', 'correlation', 'relevance'], axis=1).values
-y_train = df_train.relevance.values
+    # get model parameters
+    with open('model_param.yaml', 'r') as f:
+        params = yaml.load(f)
 
-# get model parameters
-with open('model_param.yaml', 'r') as f:
-    params = yaml.load(f)
+    # Initialize and train model
+    model = LogisticRegressionCV(**params)
+    model.fit(X_train, y_train)
 
-model = LogisticRegression(**params)
-model.fit(X_train, y_train)
-
-pickle.dump(model, open('path_to_pickle_file', 'wb'))
+    # Save trained model
+    pickle.dump(model, open('path_to_pickle_file', 'wb'))
 
 
 
